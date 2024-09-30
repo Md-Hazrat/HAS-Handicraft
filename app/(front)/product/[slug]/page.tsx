a18c65bf4e0f514241a39bbc9708b269ca9688fd
Md-Hazrat <mdhazratali8866@gmail.com>
2024-09-30 16:54:14 +0600

    working on home  page

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,13 +1,33 @@
-import data from "@/lib/data";
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import AddToCart from "@/components/products/AddToCart";
+import AddToCart from '@/components/products/AddToCart'
+import { convertDocToObj } from '@/lib/utils'
+import productService from '@/lib/services/productService'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Rating } from '@/components/products/Rating'
 
-const ProductDetails = ({ params }: { params: { slug: string } }) => {
-  const product = data.products.find((x) => x.slug === params.slug);
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService?.getBySlug(params.slug)
   if (!product) {
-    return <div>Product not found</div>;
+    return { title: 'Product not found' }
+  }
+  return {
+    title: product?.name,
+    description: product?.description,
+  }
+}
+
+export default async function ProductDetails({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService.getBySlug(params.slug)
+  if (!product) {
+    return <div>Product not found</div>
   }
   return (
     <>
@@ -17,51 +37,59 @@ const ProductDetails = ({ params }: { params: { slug: string } }) => {
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
           <Image
-            src={product.image}
-            alt={product.name}
+            src={product?.image}
+            alt={product?.name}
             width={640}
             height={640}
             sizes="100vw"
             style={{
-              width: "100%",
-              height: "auto",
+              width: '100%',
+              height: 'auto',
             }}
           ></Image>
         </div>
         <div>
           <ul className="space-y-4">
             <li>
-              <h1>{product.name}</h1>
+              <h1 className="text-xl">{product?.name}</h1>
             </li>
             <li>
-              {product.rating} of {product.numReviews} reviews
+              <Rating
+                value={product?.rating}
+                caption={`${product?.numReviews} ratings`}
+              />
             </li>
-            <li>{product.brand}</li>
+            <li> {product?.brand}</li>
             <li>
               <div className="divider"></div>
             </li>
             <li>
-              Description: <p>{product.description}</p>
+              Description: <p>{product?.description}</p>
             </li>
           </ul>
         </div>
         <div>
-          <div className="card bg-base-300 shadow-xl mt-3 md:mt-0">
+          <div className="card  bg-base-300 shadow-xl mt-3 md:mt-0">
             <div className="card-body">
               <div className="mb-2 flex justify-between">
                 <div>Price</div>
-                <div>${product.price}</div>
+                <div>${product?.price}</div>
               </div>
               <div className="mb-2 flex justify-between">
                 <div>Status</div>
                 <div>
-                  {product.countInStock > 0 ? "In Stock" : "Unavailable"}
+                  {product?.countInStock > 0 ? 'In stock' : 'Unavailable'}
                 </div>
               </div>
-              {product.countInStock !== 0 && (
-                <div className="card-actions justify-between">
+              {product?.countInStock !== 0 && (
+                <div className="card-actions justify-center">
                   <AddToCart
-                    item={{ ...product, qty: 0, color: "", size: "" }}
+                    item={{
+                      ...convertDocToObj(product),
+                      qty: 0,
+                      color: '',
+                      size: '',
+                    }}
                   />
                 </div>
               )}
@@ -70,7 +98,5 @@ const ProductDetails = ({ params }: { params: { slug: string } }) => {
         </div>
       </div>
     </>
-  );
-};
-
-export default ProductDetails;
+  )
+}
